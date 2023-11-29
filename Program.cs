707ea64074d2a6a73c98f64f9e885a8ac6271e3c
outemalu <aopheq@gmail.com>
2023-11-29 22:15:47 +0300

    final changes

--- Program.cs
+++ Program.cs
@@ -5,97 +5,93 @@ using System.Text.Json.Serialization;
 namespace JsonSerialization
 {
 
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+
     public static class MainApp
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            PersonGenerator personGenerator = new PersonGenerator();
-            personGenerator.Main(args);
+            SerializeTest serializeTest = new();
+            serializeTest.GeneratePersons();
+            serializeTest.Serialize();
+            serializeTest.ClearPersons();
+            serializeTest.Deserialize();
+            serializeTest.GetStatistics();
         }
     }
 
-    public class PersonGenerator
+    public class SerializeTest
     {
-        const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        const string Numbers = "0123456789";
-        const int PersonCount = 10000;
-        const int MinYear = 1930;
-        const int MaxYear = 2000;
+        private const string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        private const string Numbers = "0123456789";
+        private const int PersonCount = 10000;
+        private const int MinYear = 1930;
+        private const int MaxYear = 2000;
 
-        public Random Rand;
-
-        public void Main(string[] args)
+        private readonly JsonSerializerOptions _jsonOptions = new()
         {
-            Rand = new Random();
-            List<Person> clients = new List<Person>();
-            int personCount = PersonCount;
-            Console.WriteLine($"Start generation {PersonCount} persons");
-            for (int i = 0; i < personCount; i++)
-            {
-                clients.Add(CreateNewRandomPerson(i, ref i, ref personCount));
-            }
-            Console.WriteLine($"Generation {clients.Count} persons completed. Full count with children is {personCount}");
-            string fileName = $"C:\\Users\\ouch\\Desktop\\Persons.json";
-            var options = new JsonSerializerOptions
-            {
-                WriteIndented = true,
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-                Converters = { new LongToStringConverter() }
-            };
-            using FileStream createStream = File.Create(fileName);
-            JsonSerializer.Serialize(createStream, clients, options);
-            createStream.Dispose();
-        }
+            WriteIndented = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            Converters = { new LongToStringConverter(), new GenderToStringConverter() }
+        };
 
-       
+        private List<Person>? _persons;
 
-        private Person CreateNewRandomPerson(int id,ref int iterator, ref int personCount)
+        public Random Rand = new();
+
+
+        #region GenerateRandomMethods
+
+        private Person CreateNewRandomPerson(int id, ref int iterator, ref int personCount)
         {
-            Person person = new Person(
+            Person person = new(
                 id,
-                Guid.NewGuid(),       
-                GetRandomFirstName(),
-                GetRandomLastName(),
+                Guid.NewGuid(),
+                GenerateRandomFirstName(),
+                GenerateRandomLastName(),
                 id,
-                GetRandomCreditCardNumbers(),
-                GetRandomAge(),
-                GetRandomPhones(),
-                GetRandomBirthDate(MinYear, MaxYear),
-                GetRandomSalary(),
-                GetRandomIsMarred(),
-                GetRandomGender(),
-                null
+                GenerateRandomCreditCardNumbers(),
+                GenerateRandomPhones(),
+                GenerateRandomBirthDate(MinYear, MaxYear),
+                GenerateRandomSalary(),
+                GenerateRandomIsMarred(),
+                GenerateRandomGender()
                 );
-            person.Children = GetRandomChildren(id,ref iterator, ref personCount, person);
+            person.Age = GenerateRandomAge(person.BirthDate);
+            person.Children = GenerateRandomChildren(ref iterator, ref personCount, person);
             return person;
         }
 
-        private string GetRandomFirstName()
+        private string GenerateRandomFirstName()
         {
             int length = Rand.Next(3, 11);
             return new string(Enumerable.Repeat(Letters, length)
-             .Select(s => s[Rand.Next(s.Length)]).ToArray());
+                .Select(s => s[Rand.Next(s.Length)]).ToArray());
         }
 
-        private string GetRandomLastName()
+        private string GenerateRandomLastName()
         {
             int length = Rand.Next(3, 11);
             return new string(Enumerable.Repeat(Letters, length)
-             .Select(s => s[Rand.Next(s.Length)]).ToArray());
+                .Select(s => s[Rand.Next(s.Length)]).ToArray());
         }
 
-        private string[] GetRandomCreditCardNumbers()
+        private string[] GenerateRandomCreditCardNumbers()
         {
             int count = Rand.Next(1, 6);
             return Enumerable.Range(1, count)
-             .Select(x => new string(Enumerable.Repeat(Numbers, 16)
-              .Select(s => s[Rand.Next(s.Length)]).ToArray()))
-             .ToArray();
+                .Select(x => new string(Enumerable.Repeat(Numbers, 16)
+                .Select(s => s[Rand.Next(s.Length)]).ToArray()))
+                .ToArray();
         }
 
-        private int GetRandomAge() => Rand.Next(18, 99);
+        private int GenerateRandomAge(long birthDate) => (int)((((DateTime.Now.Year - DateTime.FromBinary(birthDate).Year) * 12) + (DateTime.Now.Month - DateTime.FromBinary(birthDate).Month)) / 12.0);
 
-        private string[] GetRandomPhones()
+        private string[] GenerateRandomPhones()
         {
             int length = Rand.Next(0, 6);
             string[] phones = new string[length];
@@ -107,40 +103,100 @@ namespace JsonSerialization
             return phones;
         }
 
-        private long GetRandomBirthDate(int minYear = 1930, int maxYear = 2000)
+        private long GenerateRandomBirthDate(int minYear = MinYear, int maxYear = MaxYear)
         {
             int year = Rand.Next(minYear, maxYear);
             int month = Rand.Next(1, 13);
             int day = Rand.Next(1, DateTime.DaysInMonth(year, month) + 1);
-            DateTime date = new DateTime(year, month, day);
+            DateTime date = new(year, month, day);
             long result = date.ToBinary();
             return result;
         }
 
-        private double GetRandomSalary() => Math.Round(Rand.NextDouble() * 10000, 1);
+        private double GenerateRandomSalary() => Math.Round(Rand.NextDouble() * 10000, 1);
 
-        private bool GetRandomIsMarred() => Rand.Next(0,2) == 1 ? true : false;
+        private bool GenerateRandomIsMarred() => Rand.Next(0, 2) == 1;
 
-        private Gender GetRandomGender() => Rand.Next(0,2) == 1 ? Gender.Male : Gender.Female;
+        private Gender GenerateRandomGender() => Rand.Next(0, 2) == 1 ? Gender.Male : Gender.Female;
 
-        private Child[] GetRandomChildren(int id, ref int iterator, ref int personCount, Person parent)
+        private Child[] GenerateRandomChildren(ref int iterator, ref int personCount, Person parent)
         {
             int count = Rand.Next(0, 3);
             Child[] children = new Child[count];
-            for(int i = 0; i < count; i++)
+            for (int i = 0; i < count; i++)
             {
                 children[i] = new Child(
-                    id + i + 1,
-                    GetRandomFirstName(),
-                    GetRandomLastName(),
-                    GetRandomBirthDate(DateTime.FromBinary(parent.BirthDate).Year + 18, MaxYear + 23),
-                    GetRandomGender());
+                    parent.Id + i + 1,
+                    GenerateRandomFirstName(),
+                    GenerateRandomLastName(),
+                    GenerateRandomBirthDate(DateTime.FromBinary(parent.BirthDate).Year + 18, MaxYear + 18),
+                    GenerateRandomGender());
             }
             personCount += count;
             iterator += count;
             return children;
         }
+
+        #endregion GenerateRandomMethods
+
+        public void GeneratePersons()
+        {
+            _persons = new List<Person>();
+            int personCount = PersonCount;
+            Console.WriteLine($"Start generation {PersonCount} persons");
+            for (int i = 0; i < personCount; i++)
+            {
+                _persons.Add(CreateNewRandomPerson(i, ref i, ref personCount));
+            }
+            Console.WriteLine($"Generation {_persons.Count} persons completed. Full count with children is {personCount}");
+        }
+
+        public void Serialize()
+        {
+            string fileName = $"C:\\Users\\ouch\\Desktop\\Persons.json";
+            using FileStream createStream = File.Create(fileName);
+            JsonSerializer.Serialize(createStream, _persons, _jsonOptions);
+            createStream.Dispose();
+        }
+
+        public void ClearPersons() => _persons?.Clear();
+
+        public void Deserialize()
+        {
+            string fileName = $"C:\\Users\\ouch\\Desktop\\Persons.json";
+            using FileStream readStream = File.OpenRead(fileName);
+            _persons = JsonSerializer.Deserialize<List<Person>>(readStream, _jsonOptions);
+            readStream.Dispose();
+        }
+
+        public void GetStatistics()
+        {
+            if (_persons == null)
+                return;
+            int count = 0;
+            int averageChildAge = 0;
+            int childCount = 0;
+            int creditCardCount = 0;
+            foreach (var person in _persons)
+            {
+                creditCardCount += person.CreditCardNumbers.Length;
+                count++;
+                if (person.Children.Length > 0)
+                {
+                    foreach (var child in person.Children)
+                    {
+                        var birthDate = DateTime.FromBinary(child.BirthDate);
+                        averageChildAge += (int)((((DateTime.Now.Year - birthDate.Year) * 12) + (DateTime.Now.Month - birthDate.Month)) / 12.0);
+                        childCount++;
+                    }
+                }
+            }
+            averageChildAge /= childCount;
+            Console.WriteLine($"Person count - {count}\nCredit card count - {creditCardCount}\nAverage child age - {averageChildAge}");
+        }
     }
+
+    #region JsonConverters
 
     public class LongToStringConverter : JsonConverter<long>
     {
@@ -173,21 +229,7 @@ namespace JsonSerialization
         }
     }
 
-    public class CreditCardConverter : JsonConverter<string[]>
-    {
-        public override string[] Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-        {
-            string jsonString = reader.GetString();
-            string[] cardNumbers = jsonString.Split(new[] { '-' }, StringSplitOptions.RemoveEmptyEntries);
-            return cardNumbers;
-        }
-
-        public override void Write(Utf8JsonWriter writer, string[] value, JsonSerializerOptions options)
-        {
-            string jsonString = string.Join("-", value);
-            writer.WriteStringValue(jsonString);
-        }
-    }
+    #endregion JsonConverters
 
     class Person
     {
@@ -196,7 +238,6 @@ namespace JsonSerialization
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int SequenceId { get; set; }
-        [JsonConverter(typeof(CreditCardConverter))]
         public string[] CreditCardNumbers { get; set; }
         public int Age { get; set; }
         public string[] Phones { get; set; }
@@ -209,7 +250,7 @@ namespace JsonSerialization
         public Child[] Children { get; set; }
 
         public Person(int id, Guid transportId, string firstName, string lastName, int sequenceId, string[] creditCardNumbers,
-            int age, string[] phones, long birthDate, double salary, bool isMarred, Gender gender, Child[] children)
+            string[] phones, long birthDate, double salary, bool isMarred, Gender gender)
         {
             Id = id;
             TransportId = transportId;
@@ -217,13 +258,13 @@ namespace JsonSerialization
             LastName = lastName;
             SequenceId = sequenceId;
             CreditCardNumbers = creditCardNumbers;
-            Age = age;
+            Age = 0;
             Phones = phones;
             BirthDate = birthDate;
             Salary = salary;
             IsMarred = isMarred;
             Gender = gender;
-            Children = children;
+            Children = Array.Empty<Child>();
         }
     }
 
@@ -245,11 +286,5 @@ namespace JsonSerialization
             BirthDate = birthDate;
             Gender = gender;
         }
-    }
-
-    public enum Gender
-    {
-        Male,
-        Female
     }
 }
